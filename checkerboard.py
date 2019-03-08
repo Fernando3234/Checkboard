17f786fe867e0c33f5a49411bc9ed54c89f53c90
csPython <cspython@AD25TF04NGF1J-33.local>
2019-03-08 09:43:17 -0500

    I was able to make a for loop which allowed my one square to repeat over one row then repeat that over multiple times over the columns

--- checkerboard.py
+++ checkerboard.py
@@ -4,14 +4,16 @@ from graphics import *
 
 sqSz = 50
 
-chWin = GraphWin ("Checkers", sqSz * 15, sqSz * 15)
-chWin.setCoords(0, 0, sqSz * 15, sqSz * 15)
+chWin = GraphWin ("Checkers", sqSz * 10, sqSz * 10)
+chWin.setCoords(0, 0, sqSz * 10, sqSz * 10)
 
-
-for i in range(8):
-    sQ = Rectangle(Point(sqSz, sqSz), Point(sqSz * 2, sqSz*2))
-    sQ .setFill(color_rgb(240,0,0))
-    sQ.draw(chWin)
+for j in range (8):
+    for i in range(8):
+        sQ = Rectangle(Point(sqSz *( i + 1), sqSz * (j + 1)),
+                       Point(sqSz* (i + 2), sqSz * (j + 2)))
+        sQ .setFill(color_rgb(240,0,0))
+        sQ.draw(chWin)
 
 chWin.getMouse()
 chWin.close()
+

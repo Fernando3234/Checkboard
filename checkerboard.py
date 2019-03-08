fe4b1fab1bc0f60f2d182291db45320dfd036f87
csPython <cspython@AD25TF04NGF1J-33.local>
2019-03-08 09:20:36 -0500

    Drawing First square

--- checkerboard.py
+++ checkerboard.py
@@ -1,14 +1,17 @@
 #Checkers 01 by Fernando
 from graphics import *
 
+
 sqSz = 50
 
 chWin = GraphWin ("Checkers", sqSz * 15, sqSz * 15)
 chWin.setCoords(0, 0, sqSz * 15, sqSz * 15)
 
-sQ = Rectangle(Point(sqSz, sqSz), Point(sqSz *2, sqSz*2))
-sQ .setFill(color_rgb(240,0,0))
-sQ.draw(chWin)
+
+for i in range(8):
+    sQ = Rectangle(Point(sqSz, sqSz), Point(sqSz * 2, sqSz*2))
+    sQ .setFill(color_rgb(240,0,0))
+    sQ.draw(chWin)
 
 chWin.getMouse()
 chWin.close()

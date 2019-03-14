bace74ef8d778d1ae21580e968b81b3b40c86f59
csPython <cspython@AD25TF04NGF1J-33.local>
2019-03-14 13:31:46 -0400

    Added interactivity

--- checkerboard.py
+++ checkerboard.py
@@ -1,19 +1,51 @@
 #Checkers 01 by Fernando
 from graphics import *
 
+print("""Pleasew input hwo big you want the tiles to be.
+Your answer must only contain whole numbers.
+50 is standard size""")
 
-sqSz = 50
+try:
+    sqSz = int(input("Square Sizes: "))
+except ValueError:
+    sqSz = 50
 
+def create_tile(tR, tC, color):
+    global sqSz
+    tL = Rectangle(Point(sqSz * (tR + 1), sqSz * (tC + 1)),
+                   Point(sqSz * (tR + 2), sqSz * (tC + 2)))
+    tL.setFill(color)
+    tL.draw(chWin)
+
+print (""" Please input what color you would like the checkerboard to be.
+Your input must be all lower case and no spaces.
+Black and Red is the standard color""")
+
+uC1 = input ("Color 1: ")
+if uC1 != "":
+    uC2 = input ("Color 2: ")
+else:
+    uC1 = "red"
+    uC2 = "black"
+
+    
 chWin = GraphWin ("Checkers", sqSz * 10, sqSz * 10)
 chWin.setCoords(0, 0, sqSz * 10, sqSz * 10)
 
 for j in range (8):
     for i in range(8):
-        sQ = Rectangle(Point(sqSz *( i + 1), sqSz * (j + 1)),
-                       Point(sqSz* (i + 2), sqSz * (j + 2)))
-        sQ .setFill(color_rgb(240,0,0))
-        sQ.draw(chWin)
-
+        if j % 2 == 0 :
+            if i % 2 == 0 :
+                sqCol = uC1
+            else:
+                sqCol = uC2
+        else:
+            if i %  2 == 1:
+                sqCol = uC1
+            else:
+                sqCol = uC2
+        create_tile(i, j, sqCol)
+  
 chWin.getMouse()
 chWin.close()
 
